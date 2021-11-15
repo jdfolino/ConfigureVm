7148ea80e673d765bce33948ae499e10e90c0f66
root <root@linux-machine.jf3zcjqmdslurddnzlhxxro1ba.px.internal.cloudapp.net>
2021-11-15 01:29:13 +0000

    Test

--- install_ansible.sh
+++ install_ansible.sh
@@ -3,4 +3,4 @@ apt-get install ansible
 sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install ansible --yes
